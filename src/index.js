371c049657db54a08576ed206d37f8ce65b06dd1
Tafang Joshua <tafodinho@gmail.com>
2019-09-05 11:52:38 +0100

    added toggle botton for temperature units

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ const inputForm = document.getElementById('search-box');
 
 
 inputForm.addEventListener('keypress', (e) => {
+  
   const city = document.getElementById('city-text').value;
   if (e.keyCode === 13) {
     getWeatherData(city).then((data) => {
+        console.log(city);
       renderWeatherDetails(data);
+      
     });
   }
 });
