7e45002783ecfade8ac9ad2431e62e9984e8ef41
Tafang Joshua <tafodinho@gmail.com>
2019-09-05 10:22:45 +0100

    deleted node module

--- src/index.js
+++ src/index.js
@@ -1,36 +1,6 @@
-import getWeatherData from './weather' 
+import { getWeatherData, renderWeatherDetails } from './weather' 
 
 const inputForm = document.getElementById("search-box")
-const renderWeatherDetails = (data) => {
-    const weatherDetailArea = document.getElementById("weather-info")
-    const weatherHtmlDetails = `
-        <div>
-        <div class="detail">
-            <h1>${data.main.temp}<sup>o</sup></h1>
-            <h2>${data.weather[0].description}</h2>
-            <h3>${data.name},  ${data.sys.country}</h3>
-        </div>
-        <div class="cards-section clearfix">
-            <div class="wind">
-                <img src="../assets/images/breeze.svg" alt="" height="100px" width="75px">
-                <p>${data.wind.speed} km/h</p>
-                <p>Wind</p>
-            </div>
-            <div class="humidity">
-                <img src="../assets/images/preasure.svg" alt="" height="100px" width="75px">
-                <p>${data.main.humidity}%</p>
-                <p>Humidity</p>
-            </div>
-            <div class="preasure">
-                <img src="../assets/images/humidity.svg" alt="" height="100px" width="75px">
-                <p>${data.main.pressure} Hpa</p>
-                <p>Atm. Preasure</p>
-            </div>
-        </div>
-        </div>
-    `
-    weatherDetailArea.innerHTML = weatherHtmlDetails
-}
 
 const renderSpinner = () => {
 
@@ -39,7 +9,6 @@ const renderSpinner = () => {
 inputForm.addEventListener("keypress", (e) => {
     const city = document.getElementById("city-text").value
     if(e.keyCode === 13) {
-        
         getWeatherData(city).then((data) => {
             renderWeatherDetails(data)
             console.log(data)
--- src/weather.js
+++ src/weather.js
@@ -7,4 +7,35 @@ const getWeatherData = (city) => {
             return json
         })
 }
-export default getWeatherData;
+
+const renderWeatherDetails = (data) => {
+    const weatherDetailArea = document.getElementById("weather-info")
+    const weatherHtmlDetails = `
+        <div>
+        <div class="detail">
+            <h1>${data.main.temp}<sup>o</sup></h1>
+            <h2>${data.weather[0].description}</h2>
+            <h3>${data.name},  ${data.sys.country}</h3>
+        </div>
+        <div class="cards-section clearfix">
+            <div class="wind">
+                <img src="../assets/images/breeze.svg" alt="" height="100px" width="75px">
+                <p>${data.wind.speed} km/h</p>
+                <p>Wind</p>
+            </div>
+            <div class="humidity">
+                <img src="../assets/images/preasure.svg" alt="" height="100px" width="75px">
+                <p>${data.main.humidity}%</p>
+                <p>Humidity</p>
+            </div>
+            <div class="preasure">
+                <img src="../assets/images/humidity.svg" alt="" height="100px" width="75px">
+                <p>${data.main.pressure} Hpa</p>
+                <p>Atm. Preasure</p>
+            </div>
+        </div>
+        </div>
+    `
+    weatherDetailArea.innerHTML = weatherHtmlDetails
+}
+export {getWeatherData, renderWeatherDetails};
