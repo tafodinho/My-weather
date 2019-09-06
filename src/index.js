a3e66be820135ca6e98986b6d6e2d5997cae378a
Tafang Joshua <tafodinho@gmail.com>
2019-09-06 23:16:24 +0100

    fixed changes required

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import { getWeatherData, renderWeatherDetails } from './weather';
+import { renderWeatherDetails, renderErrorPage } from './weather';
+import getWeatherData from './request';
 
 const inputForm = document.getElementById('search-box');
-
+const toggleButton = document.getElementById('toggle');
+let city = null;
+let toggle = null;
 
 inputForm.addEventListener('keypress', (e) => {
-  const city = document.getElementById('city-text').value;
-  const container = document.getElementById('weather-info');
-  const toggle = document.getElementById('toggle').checked ? 1 : 0;
-  console.log(toggle);
+  city = document.getElementById('city-text').value;
+  toggle = toggleButton.checked ? 1 : 0;
+
   if (e.keyCode === 13) {
-    getWeatherData(city).then((data) => {
-      renderWeatherDetails(data, container, toggle);
-    });
+    getWeatherData(city)
+      .then((data) => {
+        renderWeatherDetails(data, toggle);
+      })
+      .catch((error) => {
+        renderErrorPage(error);
+      });
   }
+});
+
+toggleButton.addEventListener('click', () => {
+  toggle = toggleButton.checked ? 1 : 0;
+  getWeatherData(city)
+    .then((data) => {
+      renderWeatherDetails(data, toggle);
+    })
+    .catch(() => {
+      renderErrorPage();
+    });
 });
--- src/weather.js
+++ src/weather.js
@@ -1,36 +1,26 @@
-const getWeatherData = (city) => fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e121f235d5e789b4763db2bdb5c1530e`)
-  .then((response) => response.json())
-  .then((json) => json);
+/* eslint-disable no-param-reassign */
 
-const renderWeatherDetails = (data, container, toggle) => {
-  const fahrenheit = (data.main.temp * (9 / 5)) + 32;
-  const temp = toggle ? `<h1>${fahrenheit}<sup>o</sup>F</h1>` : `<h1>${data.main.temp}<sup>o</sup>C</h1>`;
-  const weatherHtmlDetails = `
-        <div>
-        <div class="detail">
-            ${temp}
-            <h2>${data.weather[0].description}</h2>
-            <h3>${data.name},  ${data.sys.country}</h3>
-        </div>
-        <div class="cards-section clearfix">
-            <div class="wind">
-                <img src="../assets/images/breeze.svg" alt="" height="100px" width="75px">
-                <p>${data.wind.speed} km/h</p>
-                <p>Wind</p>
-            </div>
-            <div class="humidity">
-                <img src="../assets/images/preasure.svg" alt="" height="100px" width="75px">
-                <p>${data.main.humidity}%</p>
-                <p>Humidity</p>
-            </div>
-            <div class="preasure">
-                <img src="../assets/images/humidity.svg" alt="" height="100px" width="75px">
-                <p>${data.main.pressure} Hpa</p>
-                <p>Atm. Preasure</p>
-            </div>
-        </div>
-        </div>
-    `;
-  container.innerHTML = weatherHtmlDetails;
+const degreesToFahrenheit = (temp) => {
+  const result = Math.round(((temp * (9 / 5)) + 32) * 100) / 100;
+  return result;
 };
-export { getWeatherData, renderWeatherDetails };
+
+const renderWeatherDetails = (data, toggle) => {
+  const tempValue = toggle ? degreesToFahrenheit(data.main.temp) : data.main.temp;
+  const unit = toggle ? 'F' : 'C';
+  document.getElementById('unit').innerHTML = unit;
+  document.getElementById('preasure').prepend(data.main.pressure);
+  document.getElementById('weather-info').style.display = 'block';
+  document.getElementById('temp').innerHTML = tempValue;
+  document.getElementById('city').innerHTML = data.name;
+  document.getElementById('description').innerHTML = data.weather[0].description;
+  document.getElementById('country').innerHTML = data.sys.country;
+  document.getElementById('wind').innerHTML = data.wind.speed;
+  document.getElementById('humidity').innerHTML = data.main.humidity;
+  document.getElementById('preasure').innerHTML = data.main.pressure;
+};
+
+const renderErrorPage = () => {
+  document.getElementById('weather-info').innerHTML = 'Error loading location details';
+};
+export { renderWeatherDetails, renderErrorPage };
