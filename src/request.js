7484b1d3ccaa7e44b21f6338c919d5226eaafc2b
Tafang Joshua <tafodinho@gmail.com>
2019-09-10 18:47:59 +0100

    fixed linting

--- src/request.js
+++ src/request.js
@@ -1,8 +1,6 @@
-const getWeatherData = (city) => {
-  return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e121f235d5e789b4763db2bdb5c1530e`)
-    .then((response) => response.json())
-    .then((json) => json)
-    .catch((error) => error);
-};
+const getWeatherData = city => fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e121f235d5e789b4763db2bdb5c1530e`)
+  .then(response => response.json())
+  .then(json => json)
+  .catch(error => error);
 
 export default getWeatherData;
